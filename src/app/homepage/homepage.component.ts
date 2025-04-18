b752104470e02a1dda1356797937a3b25bf9b601
Kyeongmin Choi <bombard1004@kaist.ac.kr>
2025-04-18 20:35:24 +0900

    chore: Remove files

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,20 +3,15 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
-  ElementRef,
   HostListener,
   OnDestroy,
   OnInit,
-  Renderer2,
   ViewEncapsulation,
 } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { fromEvent, Subscription } from 'rxjs';
-import { debounceTime, filter } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { BasePageComponent } from './pages/page/page.component';
-
-const CARBON_WIDTH_BREAKPOINT = 1200;
 
 @Component({
   selector: 'app-homepage',
@@ -31,14 +26,9 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
   contentRef: HTMLElement;
   isMarkupReady: boolean;
 
-  private scrollSubscription: Subscription;
-  private readonly scrollDebounceTime = 100;
-
   constructor(
     private readonly cd: ChangeDetectorRef,
     private readonly router: Router,
-    private readonly elementRef: ElementRef,
-    private readonly renderer: Renderer2,
   ) {}
 
   ngOnInit(): void {
@@ -51,12 +41,6 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
         this.isSidebarOpened = false;
         this.cd.detectChanges();
       });
-
-    this.scrollSubscription = fromEvent(window, 'scroll')
-      .pipe(debounceTime(this.scrollDebounceTime))
-      .subscribe(() => {
-        this.checkViewportBoundaries();
-      });
   }
 
   ngAfterViewInit() {
@@ -67,10 +51,6 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnDestroy() {
-    if (!this.scrollSubscription) {
-      return;
-    }
-    this.scrollSubscription.unsubscribe();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -91,42 +71,6 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
     }
   }
 
-  checkViewportBoundaries() {
-    const nativeElement: HTMLElement = this.elementRef.nativeElement;
-    const footerRef: HTMLElement = nativeElement.querySelector('app-footer');
-    const newsletterRef: HTMLElement = nativeElement.querySelector(
-      '.newsletter-wrapper',
-    );
-    const carbonRef = nativeElement.querySelector('#carbonads');
-    const sponsorsWrapper = nativeElement.querySelector('.sponsors-wrapper');
-    if (!footerRef || !carbonRef) {
-      return;
-    }
-    if (window.innerWidth < 768) {
-      this.renderer.removeStyle(carbonRef, 'position');
-      this.renderer.removeStyle(carbonRef, 'bottom');
-      return;
-    }
-
-    if (carbonRef) {
-      sponsorsWrapper.classList.add('sponsors-carbon');
-    }
-
-    const isPositionFixed =
-      window.pageYOffset + window.innerHeight <
-      newsletterRef.offsetTop -
-        footerRef.offsetHeight +
-        newsletterRef.offsetHeight;
-
-    if (!isPositionFixed) {
-      this.renderer.setStyle(carbonRef, 'position', 'absolute');
-      this.renderer.setStyle(carbonRef, 'bottom', '350px');
-    } else {
-      this.renderer.removeStyle(carbonRef, 'position');
-      this.renderer.removeStyle(carbonRef, 'bottom');
-    }
-  }
-
   onRouteActivate(component: BasePageComponent) {
     if (!component) {
       return;
@@ -137,33 +81,8 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     this.contentRef = nativeElement.querySelector('.content');
-    if (this.contentRef && !this.contentRef.querySelector('.carbon-wrapper')) {
-      const scriptTag = this.createCarbonScriptTag();
-      const carbonWrapper = document.createElement('div');
-      carbonWrapper.classList.add('carbon-wrapper');
-
-      if (window.innerWidth > CARBON_WIDTH_BREAKPOINT) {
-        carbonWrapper.classList.add('hide');
-      }
-      carbonWrapper.prepend(scriptTag);
-
-      this.contentRef.prepend(carbonWrapper);
-    }
 
     this.cd.markForCheck();
-
-    // Schedule check as TOC might not be rendered yet
-    const adOverlapCheckDelay = 300;
-    setTimeout(() => this.hideAdIfTocOverflow(), adOverlapCheckDelay);
-  }
-
-  createCarbonScriptTag(): HTMLScriptElement {
-    const scriptTag = document.createElement('script');
-    scriptTag.type = 'text/javascript';
-    scriptTag.src =
-      '//cdn.carbonads.com/carbon.js?serve=CK7I653M&placement=nestjscom';
-    scriptTag.id = '_carbonads_js';
-    return scriptTag;
   }
 
   createDocSearchScriptTag(): HTMLScriptElement {
@@ -181,26 +100,5 @@ export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
       });
     };
     return scriptTag;
-  }
-
-  hideAdIfTocOverflow() {
-    const carbonHeight = 160;
-    const offset = 200;
-    const viewportHeight = window.innerHeight;
-    const maxTocHeight = viewportHeight - carbonHeight - offset;
-
-    const tocElRef = document.querySelector('.toc-wrapper ul');
-    if (!tocElRef) {
-      return;
-    }
-
-    if (
-      tocElRef.clientHeight > maxTocHeight &&
-      window.innerWidth > CARBON_WIDTH_BREAKPOINT
-    ) {
-      this.contentRef.querySelector('.carbon-wrapper').classList.add('hide');
-    } else {
-      this.contentRef.querySelector('.carbon-wrapper').classList.remove('hide');
-    }
   }
 }
